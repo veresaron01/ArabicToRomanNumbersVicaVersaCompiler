0f98dfb35fad51dce6eea94a090cbc207eeef04f
Aron Veres <veresaron01@gmail.com>
2021-03-24 13:17:00 +0100

    New approach. Not ready. RegexBuilder making in process.

--- src/com/company/CompleteARRA.java
+++ src/com/company/CompleteARRA.java
@@ -3,10 +3,7 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class CompleteARRA {
 
@@ -16,32 +13,24 @@ public class CompleteARRA {
 
     public static void main(String[] args) throws IOException {
 
-        HashSet<String> regexes = new HashSet<String>();
-        regexes.add("(M){0,5}(CM)?(D)?(C){0,3}(XC)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(C){0,3}(XC)?(X){0,3}(IV)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(C){0,3}(XL)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(C){0,3}(XC)?(X){0,3}(IX)?(I){0,3}");
+ /*       A a = new A();
+        B b = new B();
 
-        regexes.add("(M){0,5}(CM)?(D)?(C){0,3}(XC)?(X){0,3}(V)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(C){0,3}(XC)?(X){0,3}(V)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(C){0,3}(XL)?(X){0,3}(V)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(C){0,3}(XC)?(X){0,3}(V)?(I){0,3}");
+        A r = new B();
 
-        regexes.add("(M){0,5}(CD)?(D)?(C){0,3}(XC)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CD)?(D)?(C){0,3}(XC)?(X){0,3}(IV)?(I){0,3}");
-        regexes.add("(M){0,5}(CD)?(D)?(C){0,3}(XL)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CD)?(D)?(C){0,3}(XL)?(X){0,3}(IV)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CD)?(D)?(C){0,3}(XC)?(X){0,3}(V)?(I){0,3}");
-        regexes.add("(M){0,5}(CD)?(D)?(C){0,3}(XC)?(X){0,3}(V)?(I){0,3}");
-        regexes.add("(M){0,5}(CD)?(D)?(C){0,3}(XL)?(X){0,3}(V)?(I){0,3}");
-        regexes.add("(M){0,5}(CD)?(D)?(C){0,3}(XL)?(X){0,3}(V)?(I){0,3}");
+        System.out.println(a instanceof B);
+        System.out.println(b instanceof B);
+        System.out.println(r instanceof B);
+        System.out.println(r instanceof B);
+*/
 
         ArabicToRoman ar = new ArabicToRoman();
         RomanToArabic ra = new RomanToArabic();
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> regexes = ra.generateRegexes2();
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+/*
         String input;
         while (!(input = reader.readLine()).equals("exit")) {
             if (input.matches("[^[\\w&&[^MDCLXVI]]]+")){
@@ -59,6 +48,54 @@ public class CompleteARRA {
             } else if (input.matches("[\\d]+")) {
                 System.out.println(ar.atr(input));
             }
-        }
+        }*/
     }
 }
+
+
+/*
+        HashSet<String> regexes = new HashSet<String>();
+        regexes.add("(M){0,5}(CM)?(D)?(XC)?(IX)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)?(D)?(XC)?(IV)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)?(D)?(X){0,3}(IX)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)?(D)?(X){0,3}(IV)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)?(D)?(XC)?(V)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)?(D)?(X){0,3}(V)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)?(C){0,3}(XC)?(IX)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)??(C){0,3}(XC)?(IV)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)?(C){0,3}(XC)?(V)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)?(C){0,3}(X){0,3}(IX)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)??(C){0,3}(X){0,3}(IV)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CM)?(C){0,3}(X){0,3}(V)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CM)?(D)?(XL)?(X){0,3}(IX)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)?(D)?(XL)?(X){0,3}(IV)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CM)?(D)?(XL)?(X){0,3}(V)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CM)?(C){0,3}(XL)?(X){0,3}(IX)?(I){0,3}");
+        regexes.add("(M){0,5}(CM)?(C){0,3}(XL)?(X){0,3}(IV)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CM)?(C){0,3}(XL)?(X){0,3}(V)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CD)?(C){0,3}(XC)?(X){0,3}(IX)?(I){0,3}");
+        regexes.add("(M){0,5}(CD)?(C){0,3}(XC)?(X){0,3}(IV)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CD)?(C){0,3}(XC)?(X){0,3}(V)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CD)?(D)?(XC)?(X){0,3}(IX)?(I){0,3}");
+        regexes.add("(M){0,5}(CD)?(D)?(XC)?(X){0,3}(IV)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CD)?(D)?(XC)?(X){0,3}(V)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CD)?(D)?(XL)?(X){0,3}(IX)?(I){0,3}");
+        regexes.add("(M){0,5}(CD)?(D)?(XL)?(X){0,3}(IV)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CD)?(D)?(XL)?(X){0,3}(V)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CD)?(C){0,3}(XL)?(X){0,3}(IX)?(I){0,3}");
+        regexes.add("(M){0,5}(CD)?(C){0,3}(XL)?(X){0,3}(IV)?(I){0,3}");
+
+        regexes.add("(M){0,5}(CD)?(C){0,3}(XL)?(X){0,3}(V)?(I){0,3}");
+*/
