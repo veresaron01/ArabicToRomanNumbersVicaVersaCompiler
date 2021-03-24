45b4650ec762c564662fe47668adfa22ddfa71d7
Aron Veres <veresaron01@gmail.com>
2021-03-24 13:41:32 +0100

    RegexBuilder ready. Working on new ATR.

--- src/com/company/CompleteARRA.java
+++ src/com/company/CompleteARRA.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CompleteARRA {
@@ -13,24 +14,13 @@ public class CompleteARRA {
 
     public static void main(String[] args) throws IOException {
 
- /*       A a = new A();
-        B b = new B();
-
-        A r = new B();
-
-        System.out.println(a instanceof B);
-        System.out.println(b instanceof B);
-        System.out.println(r instanceof B);
-        System.out.println(r instanceof B);
-*/
-
         ArabicToRoman ar = new ArabicToRoman();
         RomanToArabic ra = new RomanToArabic();
 
-        List<String> regexes = ra.generateRegexes2();
+        List<String> regexes = ra.generateRegexes();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-/*
+
         String input;
         while (!(input = reader.readLine()).equals("exit")) {
             if (input.matches("[^[\\w&&[^MDCLXVI]]]+")){
@@ -48,54 +38,10 @@ public class CompleteARRA {
             } else if (input.matches("[\\d]+")) {
                 System.out.println(ar.atr(input));
             }
-        }*/
+        }
     }
 }
 
 
-/*
-        HashSet<String> regexes = new HashSet<String>();
-        regexes.add("(M){0,5}(CM)?(D)?(XC)?(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(XC)?(IV)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(X){0,3}(IV)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(XC)?(V)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(X){0,3}(V)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(C){0,3}(XC)?(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)??(C){0,3}(XC)?(IV)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(C){0,3}(XC)?(V)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(C){0,3}(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)??(C){0,3}(X){0,3}(IV)?(I){0,3}");
 
-        regexes.add("(M){0,5}(CM)?(C){0,3}(X){0,3}(V)?(I){0,3}");
 
-        regexes.add("(M){0,5}(CM)?(D)?(XL)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(D)?(XL)?(X){0,3}(IV)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CM)?(D)?(XL)?(X){0,3}(V)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CM)?(C){0,3}(XL)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CM)?(C){0,3}(XL)?(X){0,3}(IV)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CM)?(C){0,3}(XL)?(X){0,3}(V)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CD)?(C){0,3}(XC)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CD)?(C){0,3}(XC)?(X){0,3}(IV)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CD)?(C){0,3}(XC)?(X){0,3}(V)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CD)?(D)?(XC)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CD)?(D)?(XC)?(X){0,3}(IV)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CD)?(D)?(XC)?(X){0,3}(V)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CD)?(D)?(XL)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CD)?(D)?(XL)?(X){0,3}(IV)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CD)?(D)?(XL)?(X){0,3}(V)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CD)?(C){0,3}(XL)?(X){0,3}(IX)?(I){0,3}");
-        regexes.add("(M){0,5}(CD)?(C){0,3}(XL)?(X){0,3}(IV)?(I){0,3}");
-
-        regexes.add("(M){0,5}(CD)?(C){0,3}(XL)?(X){0,3}(V)?(I){0,3}");
-*/
--- src/com/company/RomanToArabic.java
+++ src/com/company/RomanToArabic.java
@@ -4,130 +4,6 @@ import java.util.*;
 
 public class RomanToArabic {
 
-    public LinkedHashSet<String> generateRegexes() {
-        String[] s = new String[] {"(M){0,5}","(CM)?","(CD)?","(D)?","(C){0,3}","(XC)?","(XL)?","(L)?",
-                "(X){0,3}","(IX)?", "(IV)?","(V)?","(I){0,3}"};
-        LinkedHashSet<String> regexes = new LinkedHashSet();
-        StringBuilder sb = new StringBuilder();
-
-        int i1;
-        int i2;
-        int i3;
-        for (i1 = 1; i1 <= 4; i1++) {
-            sb.append(s[0]);
-            sb.append(s[i1]);
-        }
-
-        for (i2 = 5; i2 <= 8; i2++) {
-            if (!(i1 == 1 || i1 == 2 || i1 == 4)) {
-                sb.append(s[4]);
-            }
-            sb.append(s[i2]);
-        }
-
-        for (i3 = 9; i3 <= 12; i3++) {
-            if (!(i2 == 5 || i2 == 6 || i2 == 8)) {
-                sb.append(s[8]);
-            }
-            sb.append(s[i3]);
-        }
-        regexes.add(sb.toString());
-        sb.delete(0 ,sb.length());
-
-
-        System.out.println(regexes);
-        return regexes;
-    }
-
-    public List<String> generateRegexes2() {
-        String[] s1 = new String[] {"(M){0,5}", "(CM)?", "(CD)?","(D)?"};
-        String[] s2 = new String[] {"(C){0,3}"};
-        String[] s3 = new String[] {"(XC)?", "(XL)?", "(L)?"};
-        String[] s4 = new String[] {"(X){0,3}"};
-        String[] s5 = new String[] {"(IX)?", "(IV)?", "(V)?"};
-        String[] s6 = new String[] {"(I){0,3}"};
-
-        List<String> regexes = new ArrayList();
-        List<String> partRegexes1 = new ArrayList();
-        List<String> partRegexes2 = new ArrayList();
-        List<String> partRegexes3 = new ArrayList();
-        List<String> partRegexes4 = new ArrayList();
-        List<String> partRegexes5 = new ArrayList();
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i <= 3; i++) {
-            sb.append(s1[0]);
-            sb.append(s1[i]);
-            partRegexes1.add(sb.toString());
-            sb.delete(0, sb.length());
-        }
-
-        //for  (int i = 0; i <= 1; i++){
-            for (String part1 : partRegexes1) {
-                if (!(part1.contains(s1[1]) || (part1.contains(s1[2])))) {
-                    sb.append(part1).append(s2[0]);
-                    partRegexes2.add(sb.toString());
-                    sb.delete(0, sb.length());
-                } else {
-                    sb.append(part1);
-                    partRegexes2.add(sb.toString());
-                    sb.delete(0, sb.length());
-                }
-            }
-        //}
-        //sb.delete(0, sb2.length());
-
-        for (String part2 : partRegexes2) {
-            for (int i = 0; i <= 1; i++) {
-                sb.append(part2).append(s3[i]);
-                partRegexes3.add(sb.toString());
-                sb.delete(0, sb.length());
-            }
-        }
-
-        for  (String part3 : partRegexes3){
-            //for (int i = 0; i <= 1; i++) {
-                if (!(part3.contains(s3[0]) || (part3.contains(s3[1])))) {
-                    sb.append(part3).append(s4[0]);
-                    partRegexes4.add(sb.toString());
-                    sb.delete(0, sb.length());
-                } else {
-                    sb.append(part3);
-                    partRegexes4.add(sb.toString());
-                    sb.delete(0, sb.length());
-                }
-            //}
-        }
-        //sb.delete(0, sb2.length());
-
-        for  (String part4 : partRegexes4){
-            for (int i = 0; i <= 1; i++) {
-                sb.append(part4).append(s5[i]);
-                partRegexes5.add(sb.toString());
-                sb.delete(0, sb.length());
-            }
-        }
-
-        for (String part5 : partRegexes5){
-            //for  (int i = 0; i <= 1; i++) {
-                if (!(part5.contains(s5[0]) || (part5.contains(s5[1])))) {
-                    sb.append(part5).append(s6[0]);
-                    regexes.add(sb.toString());
-                    sb.delete(0, sb.length());
-                } else {
-                    sb.append(part5);
-                    regexes.add(sb.toString());
-                    sb.delete(0, sb.length());
-                }
-            //}partRegexes5
-        }
-        //sb.delete(0, sb2.length());
-
-        System.out.println(regexes);
-        return regexes;
-    }
-
     public int romanToArabic(String rom) {
 
         Map<String, Integer> numbers = new LinkedHashMap<String, Integer>();
@@ -162,5 +38,83 @@ public class RomanToArabic {
             }
         }
         return arab;
+    }
+
+    public List<String> generateRegexes() {
+        String[] s1 = new String[] {"(M){0,5}", "(CM)?", "(CD)?","(D)?"};
+        String[] s2 = new String[] {"(C){0,3}"};
+        String[] s3 = new String[] {"(XC)?", "(XL)?", "(L)?"};
+        String[] s4 = new String[] {"(X){0,3}"};
+        String[] s5 = new String[] {"(IX)?", "(IV)?", "(V)?"};
+        String[] s6 = new String[] {"(I){0,3}"};
+
+        List<String> regexes = new ArrayList();
+        List<String> partRegexes1 = new ArrayList();
+        List<String> partRegexes2 = new ArrayList();
+        List<String> partRegexes3 = new ArrayList();
+        List<String> partRegexes4 = new ArrayList();
+        List<String> partRegexes5 = new ArrayList();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i <= 3; i++) {
+            sb.append(s1[0]);
+            sb.append(s1[i]);
+            partRegexes1.add(sb.toString());
+            sb.delete(0, sb.length());
+        }
+
+        for (String part1 : partRegexes1) {
+            if (!(part1.contains(s1[1]) || (part1.contains(s1[2])))) {
+                sb.append(part1).append(s2[0]);
+                partRegexes2.add(sb.toString());
+                sb.delete(0, sb.length());
+            } else {
+                sb.append(part1);
+                partRegexes2.add(sb.toString());
+                sb.delete(0, sb.length());
+            }
+        }
+
+        for (String part2 : partRegexes2) {
+            for (int i = 0; i <= 2; i++) {
+                sb.append(part2).append(s3[i]);
+                partRegexes3.add(sb.toString());
+                sb.delete(0, sb.length());
+            }
+        }
+
+        for  (String part3 : partRegexes3){
+            if (!(part3.contains(s3[0]) || (part3.contains(s3[1])))) {
+                sb.append(part3).append(s4[0]);
+                partRegexes4.add(sb.toString());
+                sb.delete(0, sb.length());
+            } else {
+                sb.append(part3);
+                partRegexes4.add(sb.toString());
+                sb.delete(0, sb.length());
+            }
+        }
+
+        for  (String part4 : partRegexes4){
+            for (int i = 0; i <= 2; i++) {
+                sb.append(part4).append(s5[i]);
+                partRegexes5.add(sb.toString());
+                sb.delete(0, sb.length());
+            }
+        }
+
+        for (String part5 : partRegexes5){
+            if (!(part5.contains(s5[0]) || (part5.contains(s5[1])))) {
+                sb.append(part5).append(s6[0]);
+                regexes.add(sb.toString());
+                sb.delete(0, sb.length());
+            } else {
+                sb.append(part5);
+                regexes.add(sb.toString());
+                sb.delete(0, sb.length());
+            }
+        }
+        return regexes;
     }
 }
